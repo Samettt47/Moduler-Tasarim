0194a5b85b7c2e359c753055b4aa1c45e402fcd1
Samettt47 <sametbyrktr47@gmail.com>
2023-01-24 19:56:23 +0300

    Moduler Tasarim
    Bu Uygulamada Partial View kullanilarak C# programlama dili ile Moduler Tasarım Örneği yapılmıştır ( Asp.Net Core MVC )

---
+++ ModulerTasarım/ModulerTasarım/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModulerTasarım.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.data = new List<string>  { "img1.jpg", "img2.jpg", "img3.jpg " };
+
+            object o = new object();
+            return View(o);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
